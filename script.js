f8cf67cbd4478fb430e11f4ccb0a4c05eb1d8f62
Jacques Philippe LOEMBA <105352995+Jacque004@users.noreply.github.com>
2025-01-13 16:09:28 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+
+    // Récupération des valeurs du formulaire
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const numero = document.getElementById("numero").value;
+    const message = document.getElementById("message").value;
+
+
+    // Simulation d'envoi du formulaire
+    console.log("Nom :", name);
+    console.log("Email :", email);
+    console.log("Numero :", numero)
+    console.log("Message :", message);
+
+    // Affiche une alerte
+    alert("Votre message a bien été reçu ! Merci " + name + ".");
+
+    // Réinitialise le formulaire
+    document.getElementById("contactForm").reset();
+});
